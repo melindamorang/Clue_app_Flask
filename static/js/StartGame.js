dd98f82c485d24ce92e58ca1e57497af286a05cd
Melinda <melindamorang@gmail.com>
2020-02-23 16:44:24 -0800

    Validation when starting game

---
+++ static/js/StartGame.js
@@ -0,0 +1,67 @@
+function validate(){
+
+    // Validate other players
+    var playerRows = document.getElementsByClassName("other_players");
+    var otherPlayerNames = [];
+    // Make sure that each entered player also has a number of cards entered.
+    for(var i=0, len=playerRows.length; i<len; i++){
+        var name = playerRows[i].getElementsByClassName("player_name")[0].value;
+        var num_cards = playerRows[i].getElementsByClassName("num_cards")[0].value;
+        if (name.length !== 0){
+            otherPlayerNames.push(name);
+            if (num_cards.length === 0) {
+                var text = `Enter the number of cards for each player.`
+                document.getElementById("validation").innerHTML = text;
+                return false;
+            }
+        }
+    }
+
+    // Make sure that there is at least one additional player.
+    if (otherPlayerNames.length === 0){
+        var text = `Enter at least one additional player.`
+        document.getElementById("validation").innerHTML = text;
+        return false;
+    }
+    // Make sure that no players have the same name.
+    if ((new Set(otherPlayerNames)).size !== otherPlayerNames.length){
+        var text = `Give each player a unique name.`
+        document.getElementById("validation").innerHTML = text;
+        return false;
+    }
+
+    // Validate number of cards.
+    // There are 30 cards in the game, 3 of which are the actual solution.
+    // Consequently, the total number of cards in play for each game is 27.
+    const totalCards = 27;
+  
+    // Count my cards (number of checked boxes)
+    var myCards = 0;
+    var checkboxes = document.getElementsByClassName("my_card_checkbox");
+    for(var i=0, len=checkboxes.length; i<len; i++){
+        if (checkboxes[i].checked) myCards += 1;
+    }
+    // Fail if I didn't enter any cards for myself.
+    if (myCards === 0) {
+        var text = `Enter your cards.`
+        document.getElementById("validation").innerHTML = text;
+        return false;
+    }
+
+    // Count the number of other players' cards.
+    var otherPlayerCards = 0;
+    var numCardBoxes = document.getElementsByClassName("num_cards");
+    for(var i=0, len=numCardBoxes.length; i<len; i++){
+        if (numCardBoxes[i].value.length != 0) otherPlayerCards += parseInt(numCardBoxes[i].value);
+    }
+
+    var cardsInPlay = myCards + otherPlayerCards;
+
+    // Fail if the number of cards in play is less than the total in the game.
+    if (cardsInPlay !== totalCards) {
+      var text = `The total number of cards must be ${totalCards}.`
+      document.getElementById("validation").innerHTML = text;
+      return false;
+    }
+
+};
