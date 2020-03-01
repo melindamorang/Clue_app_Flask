db3c03164824f085ef0fe01cfeb31452e446c502
Melinda <melindamorang@gmail.com>
2020-02-29 22:03:28 -0800

    Progressive disclosure when entering another player's guess

--- static/js/OtherPlayerGuess.js
+++ static/js/OtherPlayerGuess.js
@@ -1,24 +1,52 @@
 document.addEventListener('DOMContentLoaded', function (){
     var guesserCombo = document.getElementById("guesserCombo");
+    showHideGuess(guesserCombo);
+    showHideDisprovers();
     hideGuesserInDisprovers(guesserCombo);
 });
 
-function hideGuesserInDisprovers (guesserCombo) {
-    // Find the currently-selected guesser name
+function hideGuesserInDisprovers (guesserCombo, disproversDiv) {
     var guesser = guesserCombo.value;
-    // Get the Disprovers section
-    var disproversDiv = document.getElementById("Disprovers");
     // First, show all disprovers
+    var disproversDiv = document.getElementById("Disprovers");
     var disproverSubDivs = disproversDiv.getElementsByClassName("disproverCheckbox");
     for(var i=0, len=disproverSubDivs.length; i<len; i++){
-        disproverSubDivs[i].style.display = "inline";
+        showHideElement(disproverSubDivs[i], true);
     };
-    // Next, hide the current guesser's checkbox only.
-    // Find the disprover checkbox div associated with this guesser
-    var disproverCheckboxDiv = document.getElementById(guesser + "Checkbox");
-    // Get the checkbox itself
-    var disproverCheckbox = disproverCheckboxDiv.getElementsByTagName("input")[0];
-    // Hide the checkbox and uncheck it
-    disproverCheckbox.checked = false;
-    disproverCheckboxDiv.style.display = "none";
+    // If no guesser has been chosen, don't do anything else.
+    if (guesser !== "") {
+        // Next, hide the current guesser's checkbox only.
+        // Find the disprover checkbox div associated with this guesser
+        var disproverCheckboxDiv = document.getElementById(guesser + "Checkbox");
+        // Get the checkbox itself
+        var disproverCheckbox = disproverCheckboxDiv.getElementsByTagName("input")[0];
+        // Hide the checkbox and uncheck it
+        disproverCheckbox.checked = false;
+        showHideElement(disproverCheckboxDiv, false);
+    };
+}
+
+function showHideGuess(guesserCombo) {
+    var guesser = guesserCombo.value;
+    var guessDiv = document.getElementById("Guess");
+    if (guesser === "") showHideElement(guessDiv, false);
+    else showHideElement(guessDiv, true);
+}
+
+function showHideDisprovers() {
+    var guessCombos = document.getElementsByClassName("guess");
+    var disproversDiv = document.getElementById("Disprovers");
+    var shouldShow = true;
+    for(var i=0, len=guessCombos.length; i<len; i++){
+        if (guessCombos[i].value === "") {
+            shouldShow = false;
+            break;
+        };
+    };
+    showHideElement(disproversDiv, shouldShow);
+}
+
+function showHideElement(element, shouldShow) {
+    if (shouldShow) element.style.display = "inline";
+    else element.style.display = "none";
 }
