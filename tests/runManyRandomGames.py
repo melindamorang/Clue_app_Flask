23d2fc2d068e8c7f7941a5fd55dc261922cb9622
Melinda <melindamorang@gmail.com>
2020-03-07 17:15:52 -0800

    Run many invalid games

--- tests/runManyRandomGames.py
+++ tests/runManyRandomGames.py
@@ -4,11 +4,12 @@ import playRandomGame
 
 num_games = 10000
 out_file = r"E:\Code\Clue_app_Flask\tests\random_games_error_log.txt"
+play_valid_games = True
 
 for x in range(0, num_games):
     seed = random.randrange(sys.maxsize)
     try:
-        game_succeeded = playRandomGame.main(seed)
+        game_succeeded = playRandomGame.main(play_valid_games, seed)
         if not game_succeeded:
             with open(out_file, "a") as f:
                 f.write(f"{seed}\n")
